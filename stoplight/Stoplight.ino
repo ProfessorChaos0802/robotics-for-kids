c699599340f4b05758aaea8898cf8c02cb288c75
ProfessorChaos0802 <1822rhit@pm.me>
2023-03-08 22:41:30 -0500

    Complete stoplight code, not tested w/ hardware

--- stoplight/Stoplight.ino
+++ stoplight/Stoplight.ino
@@ -1,32 +1,86 @@
-#define GRN 11
-#define YLW 12
-#define RED 13
-#define STOPLIGHT_BUTTON 7
+#define WHITE 10 // Pin for White LED
+#define GRN 11 // Pin for Green LED
+#define YLW 12 // Pin for Yellow LED
+#define RED 13 // Pin for Red LED
+#define STOPLIGHT_BUTTON 9 // Pin for stoplight button
+#define POWER_BUTTON 8 // Pin for power button
+
+bool stoplightPower = false; // Initialize stoplight to off
+
+int debouceTime = 50; // Set debounce time to 50 ms
 
 void setup()
 {
+  pinMode(WHITE, OUTPUT);
   pinMode(GRN, OUTPUT);
   pinMode(YLW, OUTPUT);
   pinMode(RED, OUTPUT);
-  pinMode(STOPLIGHT_BUTTON, INPUT);
+  pinMode(STOPLIGHT_BUTTON, INPUT_PULLUP);
+  pinMode(POWER_BUTTON, INPUT_PULLUP);
 }
 
 void loop()
 {
-  digitalWrite(13, LOW);
-  digitalWrite(12, LOW);
-  digitalWrite(11, HIGH);
-  Serial.println(digitalRead(STOPLIGHT_BUTTON));
-  if (digitalRead(STOPLIGHT_BUTTON) == LOW) {
-    digitalWrite(8, HIGH);
-    digitalWrite(13, LOW);
-    digitalWrite(12, HIGH);
-    digitalWrite(11, LOW);
-    delay(2000); // Wait for 2000 millisecond(s)
-    digitalWrite(13, HIGH);
-    digitalWrite(12, LOW);
-    digitalWrite(11, LOW);
-    delay(8000); // Wait for 8000 millisecond(s)
+  if(stoplightPower){
+    runStoplight();
   }
-  delay(10); // Wait for 10 millisecond(s)
 }
+
+/**
+ * @brief Given a button to read, return if that button has been pressed or not as a boolean
+ * 
+ * @param button -> Arduino pin to read from
+ * @return true -> Button == HIGH
+ * @return false -> Button == LOW
+ */
+bool debounceButton(int button){
+  int initialButtonState = digitalRead(button); // Read the button
+  delay(debouceTime); // Wait the defined time
+  int newButtonState = digitalRead(button); // Read the button again
+
+  // If the original and new button states are the same, return the initial button state,
+  // otherwise return the new state
+  if(newButtonState == initialButtonState){
+    return (bool)initialButtonState;
+  } else {
+    return (bool)newButtonState;
+  }
+}
+
+/**
+ * @brief Checks if the POWER_BUTTON has been pressed. If pressed, toggle the power state of the stoplight
+ * 
+ */
+void powerStoplight(){
+  if(!debounceButton(POWER_BUTTON)){
+    stoplightPower = !stoplightPower;
+    digitalWrite(WHITE, stoplightPower);
+  }
+}
+
+/**
+ * @brief Runs the stoplight
+ * 
+ */
+void runStoplight(){
+  // Start with Green Light
+  digitalWrite(RED, LOW);
+  digitalWrite(YLW, LOW);
+  digitalWrite(GRN, HIGH);
+
+  if (!debounceButton(STOPLIGHT_BUTTON)) {
+    // Yellow Light for 0.75 seconds
+    digitalWrite(RED, LOW);
+    digitalWrite(YLW, HIGH);
+    digitalWrite(GRN, LOW);
+    delay(750); 
+
+    // Red Light for 2 seconds
+    digitalWrite(RED, HIGH);
+    digitalWrite(YLW, LOW);
+    digitalWrite(GRN, LOW);
+    delay(2000); 
+  }
+}
+
+
