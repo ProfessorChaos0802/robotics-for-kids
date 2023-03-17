730952f2fcbf4935ac8039cba5b7214d627e6451
ProfessorChaos0802 <1822rhit@pm.me>
2023-03-17 19:32:03 -0400

    Created power bank program with LEDs and a potentiometer

--- power-bank/PowerBank.ino
+++ power-bank/PowerBank.ino
@@ -1,3 +1,19 @@
-void setup(){};
+#define POT A0
 
-void loop(){};
+int numLeds = 13;
+
+void setup(){
+  pinMode(POT, INPUT);
+  
+  for(int i = 1; i <= 13; i++){
+    pinMode(i, OUTPUT);
+  }
+  
+}
+
+void loop(){
+  // Loops over each LED number and turns it on if the potentiometer has been turned far enough
+  for(int i = 1; i <= numLeds; i++){
+    digitalWrite(i, analogRead(POT) > (numLeds-i)*(1024/(numLeds+1)));
+  }
+}
